69f376c7e198a62eae869391d88571d021526d7b
inugami <inugamisomar@gmail.com>
2026-06-07 02:47:31 +0800

    Fix user service API URL

--- ramos-client/src/services/ArticleService.js
+++ ramos-client/src/services/ArticleService.js
@@ -1,53 +1,29 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    "http://localhost:5000/api/articles",
+  baseURL: `${import.meta.env.VITE_API_URL}/articles`,
 });
 
 // GET ARTICLES
-export const getArticles =
-  async () => {
-    const response =
-      await API.get("/");
-
-    return response.data;
-  };
+export const getArticles = async () => {
+  const response = await API.get("/");
+  return response.data;
+};
 
 // CREATE ARTICLE
-export const addArticle =
-  async (article) => {
-    const response =
-      await API.post(
-        "/",
-        article
-      );
-
-    return response.data;
-  };
+export const addArticle = async (article) => {
+  const response = await API.post("/", article);
+  return response.data;
+};
 
 // UPDATE ARTICLE
-export const updateArticle =
-  async (
-    id,
-    article
-  ) => {
-    const response =
-      await API.put(
-        `/${id}`,
-        article
-      );
-
-    return response.data;
-  };
+export const updateArticle = async (id, article) => {
+  const response = await API.put(`/${id}`, article);
+  return response.data;
+};
 
 // DELETE ARTICLE
-export const deleteArticle =
-  async (id) => {
-    const response =
-      await API.delete(
-        `/${id}`
-      );
-
-    return response.data;
-  };
+export const deleteArticle = async (id) => {
+  const response = await API.delete(`/${id}`);
+  return response.data;
+};
